4cc9b906925e0a60e58a30e44f6a1ac1db33543d
CWong <cwong@pivotal.io>
2016-06-21 17:27:39 -0400

    Task

--- app/src/androidTest/java/com/example/weatherapp/ApplicationTest.java
+++ app/src/androidTest/java/com/example/weatherapp/ApplicationTest.java
@@ -1,13 +1,28 @@
 package com.example.weatherapp;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.os.SystemClock;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.example.weatherapp.activities.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ApplicationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void waitonlaunch() {
+        SystemClock.sleep(10000);
     }
 }
